d2ac09a909a3c17915ac9ff5456038b51d652704
Marina Velimirović <Marina.Velimirovic@p3-group.com>
2023-01-17 12:32:06 +0100

    Initial commit

---
+++ src/main/java/com/primera/novena/NovenaApplication.java
@@ -0,0 +1,13 @@
+package com.primera.novena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/primera/novena/NovenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primera.novena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
